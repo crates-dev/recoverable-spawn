f94e07c04870540a83f1d99d4bcb0e8cc4a68cff
尤雨东 <root@ltpp.vip>
2025-01-10 12:36:48 +0800

    feat:v0.6.0

--- src/thread/type.rs
+++ src/thread/type.rs
@@ -1,5 +1,5 @@
 use crate::*;
-use std::any::Any;
+use std::{any::Any, sync::Arc};
 
 /// Type alias for a boxed dynamic type that implements `Any` and `Send`.
 ///
@@ -13,14 +13,14 @@ pub type BoxAnySend = Box<dyn Any + Send>;
 /// - `Err(BoxAnySend)`: Contains a boxed error value in case of a panic or failure.
 pub type SpawnResult = Result<(), BoxAnySend>;
 
-/// Alias for a boxed recoverable function.
+/// Alias for an `Arc`-wrapped recoverable function.
 ///
-/// - This type represents a boxed version of any function implementing the `RecoverableFunction` trait.
-/// - Useful for dynamic dispatch of recoverable functions in multi-threaded contexts.
-pub type BoxRecoverableFunction = Box<dyn RecoverableFunction>;
+/// - This type represents an `Arc`-wrapped version of any function implementing the `RecoverableFunction` trait.
+/// - Enables shared ownership and thread-safe usage of recoverable functions in concurrent environments.
+pub type BoxRecoverableFunction = Arc<dyn RecoverableFunction>;
 
-/// Alias for a boxed error handler function.
+/// Alias for an `Arc`-wrapped error handler function.
 ///
-/// - This type represents a boxed version of any function implementing the `ErrorHandlerFunction` trait.
-/// - Useful for dynamically handling errors with custom logic.
-pub type BoxErrorHandlerFunction = Box<dyn ErrorHandlerFunction>;
+/// - This type represents an `Arc`-wrapped version of any function implementing the `ErrorHandlerFunction` trait.
+/// - Allows shared ownership and thread-safe handling of errors with custom logic across multiple threads.
+pub type BoxErrorHandlerFunction = Arc<dyn ErrorHandlerFunction>;
