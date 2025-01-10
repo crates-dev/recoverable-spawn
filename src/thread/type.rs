7fc88a23ecf292ceb5e3be41545172289d1f1238
尤雨东 <root@ltpp.vip>
2025-01-10 12:31:28 +0800

    feat:v0.5.0

--- src/thread/type.rs
+++ src/thread/type.rs
@@ -1,3 +1,4 @@
+use crate::*;
 use std::any::Any;
 
 /// Type alias for a boxed dynamic type that implements `Any` and `Send`.
@@ -11,3 +12,15 @@ pub type BoxAnySend = Box<dyn Any + Send>;
 /// - `Ok(())`: Indicates successful execution of the function.
 /// - `Err(BoxAnySend)`: Contains a boxed error value in case of a panic or failure.
 pub type SpawnResult = Result<(), BoxAnySend>;
+
+/// Alias for a boxed recoverable function.
+///
+/// - This type represents a boxed version of any function implementing the `RecoverableFunction` trait.
+/// - Useful for dynamic dispatch of recoverable functions in multi-threaded contexts.
+pub type BoxRecoverableFunction = Box<dyn RecoverableFunction>;
+
+/// Alias for a boxed error handler function.
+///
+/// - This type represents a boxed version of any function implementing the `ErrorHandlerFunction` trait.
+/// - Useful for dynamically handling errors with custom logic.
+pub type BoxErrorHandlerFunction = Box<dyn ErrorHandlerFunction>;
