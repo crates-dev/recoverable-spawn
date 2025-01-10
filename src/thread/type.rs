51475afda26ce2c67e940ee5dd9fdb5034035ca5
尤雨东 <root@ltpp.vip>
2025-01-10 12:39:06 +0800

    feat:v0.7.0

--- src/thread/type.rs
+++ src/thread/type.rs
@@ -17,10 +17,10 @@ pub type SpawnResult = Result<(), BoxAnySend>;
 ///
 /// - This type represents an `Arc`-wrapped version of any function implementing the `RecoverableFunction` trait.
 /// - Enables shared ownership and thread-safe usage of recoverable functions in concurrent environments.
-pub type BoxRecoverableFunction = Arc<dyn RecoverableFunction>;
+pub type ArcRecoverableFunction = Arc<dyn RecoverableFunction>;
 
 /// Alias for an `Arc`-wrapped error handler function.
 ///
 /// - This type represents an `Arc`-wrapped version of any function implementing the `ErrorHandlerFunction` trait.
 /// - Allows shared ownership and thread-safe handling of errors with custom logic across multiple threads.
-pub type BoxErrorHandlerFunction = Arc<dyn ErrorHandlerFunction>;
+pub type ArcErrorHandlerFunction = Arc<dyn ErrorHandlerFunction>;
