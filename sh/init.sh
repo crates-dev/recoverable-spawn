8848b30fecf3a32aa22ad5aee5beccc08b068fdf
尤雨东 <root@ltpp.vip>
2025-02-07 23:10:21 +0800

    feat:

--- sh/init.sh
+++ sh/init.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-git remote add jihulab git@jihulab.com:ltpp-universe/recoverable-spawn.git;
+git init;
+git config core.ignorecase false;
+git remote add gitee git@gitee.com:ltpp-universe/recoverable-spawn.git;
 git remote add origin git@github.com:ltpp-universe/recoverable-spawn.git;
